fdcf4e9cbe924ed4d0d3e877d7577b55f4703f5d
peppone-choi <peppone.choi@gmail.com>
2024-03-08 19:45:19 +0900

    Title: 실수 입력받아 계산 2, Time: 144ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240308/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double a = sc.nextDouble();
+        System.out.println(String.format("%.2f", a + 1.5));
+    }
+}

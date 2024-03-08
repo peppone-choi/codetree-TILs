d7f5b2780b4cfa55db3ac3669009eeb7f90614c0
peppone-choi <peppone.choi@gmail.com>
2024-03-08 19:04:14 +0900

    Title: 변수 출력하기 3, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240308/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        String c = "C";
+        System.out.println(String.format("%d->%d->%s", a, b, c));
+    }
+}
